9aab1da9ff3fcfd49a281661899a7b73f7a4dbb9
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-10-21 16:52:57 +0200

    Added event on addMemberButton

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const cardContainer = document.querySelector(".team-container");
+const addBtn = document.getElementById("addMemberButton");
 
 const names = ["Wayne Barnett", "Angela Caroll", "Walter Gordon", "Angela Lopez", "Scott Estrada", "Barbara Ramos"];
 const roles = ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"];
@@ -6,18 +7,54 @@ const imgs = ["wayne-barnett-founder-ceo.jpg", "angela-caroll-chief-editor.jpg",
 
 
 let team = [];
+let member = {
+    "name": "", 
+    "role": "",
+    "imgSrc": "",
+    "imgAlt": ""
+};
+
 for (let i = 0; i < names.length; i++) {
     
-    const currentMember = {
+    let currentMember = member;
+    /* currentMember = {
         "name": `${names[i]}`,
         "role": `${roles[i]}`,
         "imgSrc" : `img/${imgs[i]}`,
         "imgAlt" : `${names[i]}`
-    }
+    } */
     
+    currentMember.name = `${names[i]}`;
+    currentMember.role = `${roles[i]}`;
+    currentMember.imgSrc = `img/${imgs[i]}`;
+    currentMember.imgAlt =  `${names[i]}`;
+
     team.push(currentMember);
     
+    createCard(currentMember);
+   
+
+}
+
+
+addBtn.addEventListener("click", function(){
+    member.name = document.getElementById("name").value;
+    //console.log(newMemberName);
+    member.role = document.getElementById("role").value;
+
+    let imageSource = document.getElementById("image").value
+    member.imgSrc = `img/${imageSource}`;
+    member.imgAlt = member.name;
+
+    console.log(member);
     
+    createCard(member);
+
+})
+
+
+function createCard(currentMember){
+     
     const teamCard = document.createElement("div");
     teamCard.classList.add("team-card");
     
@@ -38,8 +75,4 @@ for (let i = 0; i < names.length; i++) {
     teamCard.append(cardImg);
     teamCard.append(cardTxt);
     cardContainer.append(teamCard)
-
-    
-    
-   
 }
