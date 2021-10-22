2161cfd297aca22a6ed8c1d2d6a650ad30c54248
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-10-22 13:34:15 +0200

    Changed script using array of object

--- js/script_no_array.js
+++ js/script_no_array.js
@@ -1,73 +1,50 @@
 const cardContainer = document.querySelector(".team-container");
 const addBtn = document.getElementById("addMemberButton");
 
-const team = [
-    {
-        name: "Wayne Barnett",
-        role: "Founder & CEO",
-        imgSrc: "img/wayne-barnett-founder-ceo.jpg",
-        imgAlt: "Wayne Barnett"
-    },
-
-    {
-        name: "Angela Caroll",
-        role: "Chief Editor",
-        imgSrc: "img/angela-caroll-chief-editor.jpg",
-        imgAlt: "Angela Caroll"
-    },
-
-    {
-        name: "Walter Gordon",
-        role: "Office Manager",
-        imgSrc: "img/walter-gordon-office-manager.jpg",
-        imgAlt: "Walter Gordon"
-    },
-
-    {
-        name: "Angela Lopez",
-        role: "Social Media Manager",
-        imgSrc: "img/angela-lopez-social-media-manager.jpg",
-        imgAlt: "Angela Lopez"
-    },
-
-    {
-        name: "Scott Estrada",
-        role: "Developer",
-        imgSrc: "img/scott-estrada-developer.jpg",
-        imgAlt: "Scott Estrada"
-    },
-
-    {
-        name: "Barbara Ramos",
-        role: "Graphic Designer",
-        imgSrc: "img/barbara-ramos-graphic-designer.jpg",
-        imgAlt: "Barbara Ramos"
-    }
-]
+const names = ["Wayne Barnett", "Angela Caroll", "Walter Gordon", "Angela Lopez", "Scott Estrada", "Barbara Ramos"];
+const roles = ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"];
+const imgs = ["wayne-barnett-founder-ceo.jpg", "angela-caroll-chief-editor.jpg", "walter-gordon-office-manager.jpg", "angela-lopez-social-media-manager.jpg", "scott-estrada-developer.jpg", "barbara-ramos-graphic-designer.jpg"]
 
 
+let team = [];
+let member = {
+    "name": "", 
+    "role": "",
+    "imgSrc": "",
+    "imgAlt": ""
+};
 
-for (let i = 0; i < team.length; i++) {
+for (let i = 0; i < names.length; i++) {
     
-    let currentMember = team[i];
-    //console.log(currentMember);
+    let currentMember = member;
+    /* currentMember = {
+        "name": `${names[i]}`,
+        "role": `${roles[i]}`,
+        "imgSrc" : `img/${imgs[i]}`,
+        "imgAlt" : `${names[i]}`
+    } */
+    
+    currentMember.name = `${names[i]}`;
+    currentMember.role = `${roles[i]}`;
+    currentMember.imgSrc = `img/${imgs[i]}`;
+    currentMember.imgAlt =  `${names[i]}`;
+
+    team.push(currentMember);
+    
     createCard(currentMember);
    
 
 }
 
 
-
-
 addBtn.addEventListener("click", function(){
-    let member = {};
 
     member.name = document.getElementById("name").value;
+    //console.log(newMemberName);
     member.role = document.getElementById("role").value;
 
     let imageSource = document.getElementById("image").value
-
-    member.imgSrc = imagePath(imageSource);
+    member.imgSrc = `img/${imageSource}`;
     member.imgAlt = member.name;
 
     console.log(member);
@@ -113,19 +90,4 @@ function createCard(currentMember){
     teamCard.append(cardImg);
     teamCard.append(cardTxt);
     cardContainer.append(teamCard)
-}
-
-
-
-/**
- * Restituisce il percorso dell'immagine scritto nel form, distinguendo i casi da percorso interno o esterno
- * @param {string} src - percorso del file
- * @returns 
- */
-function imagePath(src) {
-    if (!src.startsWith("http")){
-        return `img/${src}`;
-    } else {
-        return src
-    }
 }
